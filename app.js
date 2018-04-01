7ae6b840cf85364cdf43e97a6be29f09d24781b9
workraghwendra <raghwendrapratap0194@gmail.com>
2018-04-01 19:04:47 +0530

    error handler

--- app.js
+++ app.js
@@ -52,15 +52,26 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(req, res) {
+	res.status(400);
+	res.render('./partials/400', {title: 'Bad Request'});
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Handle 404
+app.use(function(req, res) {
+	res.status(404);
+	res.render('./partials/404', {title: 'Not found'});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+	res.status(500);
+	res.render('./partials/500', {title:'Internal Server Error', error: error});
+});
+// Handle 503
+app.use(function(error, req, res, next) {
+	res.status(503);
+	res.render('./partials/503', {title:'Service Unavailable', error: error});
 });
 
 module.exports = app;
